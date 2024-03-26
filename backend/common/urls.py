a0f73ce06a2ebb472f1cb20093a06308d233a178
ethanoutangoun <ethanoutangoun@gmail.com>
2024-03-26 15:58:44 -0700

    simple get request

--- backend/common/urls.py
+++ backend/common/urls.py
@@ -6,5 +6,6 @@ app_name = "common"
 urlpatterns = [
     # Define your existing view for the root path
     path("", views.IndexView.as_view(), name="index"),
+    path("get-data/", views.get_data, name="get_data"),
     path("admin/", admin.site.urls),
 ]
