be275057652d3eab04f9b1a15e50df65943a10bd
ethanoutangoun <ethanoutangoun@gmail.com>
2024-03-27 23:12:43 -0700

    add many to many with restaurant

--- backend/equipment/models.py
+++ backend/equipment/models.py
@@ -8,5 +8,16 @@ class Equipment(models.Model):
     serialNumber = models.CharField(max_length=100, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True) 
 
+
+    def __str__(self):
+        return self.name
+    
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    equipment = models.ManyToManyField(Equipment, related_name='equipment', blank=True)
+
     def __str__(self):
         return self.name
